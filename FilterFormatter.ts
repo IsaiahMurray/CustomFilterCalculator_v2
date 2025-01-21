05eae4744cbef2f7fa03a73f65399778ed9103ee
Isaiah Murray <murray.isaiah13@gmail.com>
2025-01-21 10:39:13 -0500

    Made filter interface

--- FilterFormatter.ts
+++ FilterFormatter.ts
@@ -1,1 +1,12 @@
+import * as fs from "fs";
+import * as path from "path";
+
+interface Filter {
+  length: number;
+  width: number;
+  height: number;
+  orientation: "portrait" | "landscape";
+  price: number;
+  filterID: string;
+}
 
