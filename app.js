1ccc004d6f867a1a7e8afa3a02e115ed3b2e0fe0
Isaiah Murray <murray.isaiah13@gmail.com>
2025-02-05 11:14:22 -0500

    Redoing logic
    -Made function to calculate split filter dimensions and rite them to a file

--- app.js
+++ app.js
@@ -1,25 +1,124 @@
-var _this = this;
-var filter = {
-    length: 10,
-    width: 20,
-    height: 5,
-    orientation: "portrait",
-    price: 50,
-    getId: function () {
-        return "".concat(_this.length, "x").concat(_this.width, "x").concat(_this.height);
-    },
-    getVerticalSplit: function () {
-        return "".concat(_this.length / 2, "x").concat(_this.width, "x").concat(_this.height);
-    },
-    getHorizontalSplit: function () {
-        return "".concat(_this.length, "x").concat(_this.width / 2, "x").concat(_this.height);
-    },
-};
-console.log(filter.getVerticalSplit()); // Output: "5x20x5"
-// let filters: Filter[] = [];
-// async function loadFilters() {
-//   const response = await fetch("filters.json");
-//   filters = await response.json();
-// }
-// loadFilters();
-console.log("Hi");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+function splitFilters(inputFile, outputFile) {
+    var data = JSON.parse(fs.readFileSync(inputFile, "utf-8"));
+    var splitData = [];
+    // First split by length
+    data.forEach(function (filter) {
+        var _a;
+        var newLength = filter.length / 2;
+        var newWidth = filter.width;
+        var newOrientation = filter.orientation;
+        if (newLength < 4)
+            return; // Skip if new length is less than 4 inches
+        if (newLength > newWidth) {
+            _a = [newLength, newWidth], newWidth = _a[0], newLength = _a[1];
+            newOrientation = newOrientation === "portrait" ? "landscape" : "portrait";
+        }
+        splitData.push({
+            length: newLength,
+            width: newWidth,
+            height: filter.height,
+            orientation: newOrientation,
+            price: filter.price,
+            filterID: "".concat(newLength, "x").concat(newWidth, "x").concat(filter.height),
+            originalFilterID: filter.filterID,
+            qty: 2,
+        });
+    });
+    // Then split by width
+    splitData = splitData
+        .flatMap(function (filter) {
+        var _a;
+        var newWidth = filter.width / 2;
+        var newLength = filter.length;
+        var newOrientation = filter.orientation;
+        if (newWidth < 4)
+            return [filter]; // Skip if new width is less than 4 inches
+        if (newLength > newWidth) {
+            _a = [newLength, newWidth], newWidth = _a[0], newLength = _a[1];
+            newOrientation =
+                newOrientation === "portrait" ? "landscape" : "portrait";
+        }
+        return [
+            filter,
+            {
+                length: newLength,
+                width: newWidth,
+                height: filter.height,
+                orientation: newOrientation,
+                price: filter.price,
+                filterID: "".concat(newLength, "x").concat(newWidth, "x").concat(filter.height),
+                originalFilterID: filter.originalFilterID,
+                qty: 2,
+            },
+        ];
+    })
+        .filter(Boolean); // Remove any undefined entries
+    // Remove duplicates
+    var uniqueFilters = new Map();
+    splitData.forEach(function (filter) {
+        uniqueFilters.set(filter.filterID, filter);
+    });
+    fs.writeFileSync(outputFile, JSON.stringify(Array.from(uniqueFilters.values()), null, 2));
+    console.log("File saved as ".concat(outputFile));
+}
+splitFilters("filters1Inch.json", "splitFilters1Inch.json");
+/* Files
+! filters.json
+ {
+    "length": 10,
+    "width": 10,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 2.4,
+    "filterID": "10x10x1"
+  },
+
+! splitFilters.json
+ {
+    "length": 5,
+    "width": 10,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 2.4,
+    "filterID": "5.10.1"
+    "originalFilterID": "10x10x1"
+    "qty": 2
+  },
+! triSplitFilters.json
+{
+    "length": 10,
+    "width": 16,
+    "height": 1,
+    "orientation": "horizontal",
+    "price": 3.79,
+    "filterID": "10x16x1"
+    "originalFilterID": "16x30x1"
+    "qty": 3
+  },
+  {
+    "length": 10,
+    "width": 16,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 3.79,
+    "filterID": "5.33x30x1"
+    "originalFilterID": "16x30x1"
+    "qty": 3
+  },
+
+! quadSplitFilters.json
+{
+    "length": 5,
+    "width": 5,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 3.81,
+    "filterID": "5x5x1"
+    "originalFilterID": "20x20x1"
+    "qty": 4
+  },
+
+*/
--- app.ts
+++ app.ts
@@ -1,44 +1,148 @@
-interface IFilter {
+import * as fs from "fs";
+
+interface Filter {
   length: number;
   width: number;
   height: number;
   orientation: string;
   price: number;
+  filterID: string;
 }
 
-interface Filter extends IFilter {
-  getId: () => string;
-  getVerticalSplit: () => string;
-  getHorizontalSplit: () => string;
-
+interface SplitFilter extends Filter {
+  originalFilterID: string;
+  qty: number;
 }
 
-const filter: Filter = {
-  length: 10,
-  width: 20,
-  height: 5,
-  orientation: "portrait",
-  price: 50,
-  getId: () => {
-    return `${this.length}x${this.width}x${this.height}`;
+function splitFilters(inputFile: string, outputFile: string): void {
+  const data: Filter[] = JSON.parse(fs.readFileSync(inputFile, "utf-8"));
+  let splitData: SplitFilter[] = [];
+
+  // First split by length
+  data.forEach((filter) => {
+    let newLength = filter.length / 2;
+    let newWidth = filter.width;
+    let newOrientation = filter.orientation;
+
+    if (newLength < 4) return; // Skip if new length is less than 4 inches
+
+    if (newLength > newWidth) {
+      [newWidth, newLength] = [newLength, newWidth];
+      newOrientation = newOrientation === "portrait" ? "landscape" : "portrait";
+    }
+
+    splitData.push({
+      length: newLength,
+      width: newWidth,
+      height: filter.height,
+      orientation: newOrientation,
+      price: filter.price,
+      filterID: `${newLength}x${newWidth}x${filter.height}`,
+      originalFilterID: filter.filterID,
+      qty: 2,
+    });
+  });
+
+  // Then split by width
+  splitData = splitData
+    .flatMap((filter) => {
+      let newWidth = filter.width / 2;
+      let newLength = filter.length;
+      let newOrientation = filter.orientation;
+
+      if (newWidth < 4) return [filter]; // Skip if new width is less than 4 inches
+
+      if (newLength > newWidth) {
+        [newWidth, newLength] = [newLength, newWidth];
+        newOrientation =
+          newOrientation === "portrait" ? "landscape" : "portrait";
+      }
+
+      return [
+        filter,
+        {
+          length: newLength,
+          width: newWidth,
+          height: filter.height,
+          orientation: newOrientation,
+          price: filter.price,
+          filterID: `${newLength}x${newWidth}x${filter.height}`,
+          originalFilterID: filter.originalFilterID,
+          qty: 2,
+        },
+      ];
+    })
+    .filter(Boolean); // Remove any undefined entries
+
+  // Remove duplicates
+  const uniqueFilters = new Map();
+  splitData.forEach((filter) => {
+    uniqueFilters.set(filter.filterID, filter);
+  });
+
+  fs.writeFileSync(
+    outputFile,
+    JSON.stringify(Array.from(uniqueFilters.values()), null, 2)
+  );
+  console.log(`File saved as ${outputFile}`);
+}
+
+splitFilters("filters1Inch.json", "splitFilters1Inch.json");
+
+/* Files
+! filters.json
+ {
+    "length": 10,
+    "width": 10,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 2.4,
+    "filterID": "10x10x1"
   },
-  getVerticalSplit: () => {
-    return `${this.length / 2}x${this.width}x${this.height}`;
+
+! splitFilters.json
+ {
+    "length": 5,
+    "width": 10,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 2.4,
+    "filterID": "5.10.1"
+    "originalFilterID": "10x10x1"
+    "qty": 2
   },
-  getHorizontalSplit: () => {
-    return `${this.length}x${this.width / 2}x${this.height}`;
+! triSplitFilters.json
+{
+    "length": 10,
+    "width": 16,
+    "height": 1,
+    "orientation": "horizontal",
+    "price": 3.79,
+    "filterID": "10x16x1"
+    "originalFilterID": "16x30x1"
+    "qty": 3
   },
-};
+  {
+    "length": 10,
+    "width": 16,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 3.79,
+    "filterID": "5.33x30x1"
+    "originalFilterID": "16x30x1"
+    "qty": 3
+  },
 
-console.log(filter.getVerticalSplit()); // Output: "5x20x5"
+! quadSplitFilters.json
+{
+    "length": 5,
+    "width": 5,
+    "height": 1,
+    "orientation": "portrait",
+    "price": 3.81,
+    "filterID": "5x5x1"
+    "originalFilterID": "20x20x1"
+    "qty": 4
+  },
 
-// let filters: Filter[] = [];
-
-// async function loadFilters() {
-//   const response = await fetch("filters.json");
-//   filters = await response.json();
-// }
-
-// loadFilters();
-
-console.log("Hi")
+*/
